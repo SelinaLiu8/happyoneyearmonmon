71c173dfde2e23afe98abba6431c6c16377790bd
selinal <selnal@umich.edu>
2023-01-05 20:58:44 -0500

    almost done

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 const paper4 = document.querySelector("#p4");
 const paper5 = document.querySelector("#p5");
-const paper6 = document.querySelector("#p6");
+// const paper6 = document.querySelector("#p6");
 // const paper7 = document.querySelector("#p7");
 // const paper8 = document.querySelector("#p8");
 // const paper9 = document.querySelector("#p9");
@@ -20,7 +20,7 @@ nextBtn.addEventListener("click", goNextPage);
 
 // business logic
 let currentLocation = 1;
-let numPapers = 7;
+let numPapers = 6;
 let maxLocation = numPapers + 1;
 
 function openBook() {
@@ -70,10 +70,10 @@ function goNextPage() {
                 paper5.classList.add("flipped");
                 paper5.style.zIndex = 5;
                 break;
-            case 6:
-                paper6.classList.add("flipped");
-                paper6.style.zIndex = 6;
-                break;
+            // case 6:
+            //     paper6.classList.add("flipped");
+            //     paper6.style.zIndex = 6;
+            //     break;
             // case 7:
             //     paper7.classList.add("flipped");
             //     paper7.style.zIndex = 7;
@@ -86,10 +86,10 @@ function goNextPage() {
             //     paper9.classList.add("flipped");
             //     paper9.style.zIndex = 9;
             //     break;
-            case 7:
+            case 6:
                 closeBook();
                 paper10.classList.add("flipped");
-                paper10.style.zIndex = 7;
+                paper10.style.zIndex = 6;
                 // document.getElementById("book").style.boxShadow = "0px 0px 0px gray";
                 break;
             default:
@@ -105,28 +105,28 @@ function goPrevPage() {
             case 2:
                 closeBook();
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 7;
+                paper1.style.zIndex = 6;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 6;
+                paper2.style.zIndex = 5;
                 break;
             case 4:
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 5;
+                paper3.style.zIndex = 4;
                 break;
             case 5:
                 paper4.classList.remove("flipped");
-                paper4.style.zIndex = 4;
+                paper4.style.zIndex = 3;
                 break;
             case 6:
                 paper5.classList.remove("flipped");
-                paper5.style.zIndex = 3;
+                paper5.style.zIndex = 2;
                 break;
-            case 7:
-                paper6.classList.remove("flipped");
-                paper6.style.zIndex = 2;
-                break;
+            // case 7:
+            //     paper6.classList.remove("flipped");
+            //     paper6.style.zIndex = 2;
+            //     break;
             // case 8:
             //     paper7.classList.remove("flipped");
             //     paper7.style.zIndex = 4;
@@ -139,7 +139,7 @@ function goPrevPage() {
             //     paper9.classList.remove("flipped");
             //     paper9.style.zIndex = 2;
             //     break;
-            case 8:
+            case 7:
                 paper10.classList.remove("flipped");
                 paper10.style.zIndex = 1;
                 openBook();
